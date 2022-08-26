d575b71d72e2b75c76236790d722e9d91e18ce4a
siam786 <shohrab.hossain36@gmail.com>
2022-08-26 10:22:24 +0600

    add first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import Counter from './Components/Counter'
+import Stats from './Components/Stats'
+import { useState } from 'react'
+
+const initialState = [
+  {
+    id: 1,
+    count: 0,
+  },
+  { id: 2, count: 0 },
+  { id: 3, count: 0 },
+]
 
 function App() {
+  const [state, setState] = useState(initialState)
+
+  const totalCount = () => {
+    return state.reduce((total, counter) => total + counter.count, 0)
+  }
+
+  const increment = (id) => {
+    const updatedCounter = state.map((c) => {
+      if (c.id === id) {
+        return {
+          ...c,
+          count: c.count + 1,
+        }
+      }
+      return { ...c }
+    });
+    setState(updatedCounter);
+  }
+  const decrement = (id) => {
+    const updatedCounter = state.map((c) => {
+      if (c.id === id) {
+        return {
+          ...c,
+          count: c.count + 1,
+        }
+      }
+      return { ...c }
+    });
+    setState(updatedCounter);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+      <h1 class="max-w-md mx-auto text-center text-2xl font-bold">
+        Simple Counter Application
+      </h1>
+
+      <div class="max-w-md mx-auto mt-10 space-y-5">
+        {state.map((count) => (
+          <Counter key={count.id} id={count.id} count={count.count} increment={increment} decrement={decrement} />
+        ))}
+
+        <Stats count={totalCount()} />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Components/Count.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Count({count}) {
+  return (
+    <div className="text-2xl font-semibold">
+        {count}
+    </div>
+  )
+}
